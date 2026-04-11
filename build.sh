905c4e713c249f1a9e247e0a7b5bfa6342be6456
idunmore <62761638+idunmore@users.noreply.github.com>
2026-04-11 16:27:06 -0400

    Add basic build script

---
+++ build.sh
@@ -0,0 +1,44 @@
+#!/bin/zsh
+
+Help()
+{
+	# Display Help
+	echo "RetroProbe - build.sh Script"
+	echo
+	echo "Syntax: build.sh [clean|h|help]"
+	echo "  options:"
+	echo "    clean       - Perform a clean build (destroys dist/)."
+	echo "    -h | --help - Displays this Help info."
+	echo
+}
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+	Help
+	exit
+fi
+
+# Clean the dist/ folder if requested
+if [ "$1" = "clean" ]; then
+	echo "Cleaning ..."
+	cd dist/
+	rm -rf *
+	cd ..	
+fi
+
+# The build is simply copying and/or compiling files from their origins to
+# dist/ following a structure that can be directly copied to the Circuit
+# Python file systems
+
+# Do libraries
+mkdir -p dist/lib
+cp lib/adafruit_displayio_ssd1306.mpy dist/lib/adafruit_displayio_ssd1306.mpy
+cp lib/adafruit_framebuf.mpy dist/lib/adafruit_framebuf.mpy
+cp lib/adafruit_ssd1306.mpy dist/lib/adafruit_ssd1306.mpy
+
+# Do fonts
+cp fonts/font5x8.bin dist/font5x8.bin
+
+# Do code
+cp src/code.py dist/code.py
+mpy-cross src/retroprobe.py -o dist/retroprobe.mpy
+exit
