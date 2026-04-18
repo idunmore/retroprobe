a5335df1bfcc43a8b7720838768e3caab4989c2a
idunmore <62761638+idunmore@users.noreply.github.com>
2026-04-18 15:42:24 -0400

    Switch to side-by-side presentation

--- src/intv_controllers.py
+++ src/intv_controllers.py
@@ -19,38 +19,16 @@ import time
 # Retroprobe Modules
 import db9_port_probe
 import sprites
-from drawing_primitives import draw_filled_circle
+from drawing_primitives import filled_circle, bevelled_rect, filled_bevelled_rect
 
-# Character Sprites (5x3)
-sp_zero = bytearray([0b11111000, 0b10001000, 0b11111000])
-sp_one = bytearray([0b10010000,	0b11111000,	0b10000000])
-sp_two = bytearray([0b11101000,	0b10101000,	0b10111000])
-sp_three = bytearray([0b10001000, 0b10101000, 0b11111000])
-sp_four = bytearray([0b00111000, 0b00100000, 0b11111000])
-sp_five = bytearray([0b10111000, 0b10101000, 0b11101000])
-sp_six = bytearray([0b11111000, 0b10101000,	0b11101000])
-sp_seven = bytearray([0b00001000, 0b11101000, 0b00011000])
-sp_eight = bytearray([0b11111000, 0b10101000, 0b11111000])
-sp_nine = bytearray([0b10111000, 0b10101000, 0b11111000])
-sp_clear = bytearray([0b11111000, 0b10001000, 0b10001000])
-sp_enter = bytearray([0b11111000, 0b10101000, 0b10001000])
+# Constants
+KEYPAD_X_OFFSET = 10
+LEFT_ACTION_X_OFFSET = 2 
+RIGHT_ACTION_X_OFFSET = 48
+DISC_X_OFFSET = 90
+DISC_Y_OFFSET = 24
 
-spr_zero = sprites.Sprite(sp_zero, 8, 3)
-spr_one = sprites.Sprite(sp_one, 8, 3)
-spr_two = sprites.Sprite(sp_two, 8, 3)
-spr_three = sprites.Sprite(sp_three, 8, 3)
-spr_four = sprites.Sprite(sp_four, 8, 3)
-spr_five = sprites.Sprite(sp_five, 8, 3)
-spr_six = sprites.Sprite(sp_six, 8, 3)
-spr_seven = sprites.Sprite(sp_seven, 8, 3)
-spr_eight = sprites.Sprite(sp_eight, 8, 3)
-spr_nine = sprites.Sprite(sp_nine, 8, 3)
-spr_clear = sprites.Sprite(sp_clear, 8, 3)
-spr_enter = sprites.Sprite(sp_enter, 8, 3)
-
-matrix = [spr_clear, spr_seven, spr_four, spr_one,
-          spr_zero, spr_eight,  spr_five, spr_two,
-          spr_enter, spr_nine, spr_six, spr_three]
+matrix = ['1', '4', '7', 'C', '2', '5', '8', '0', '3', '6', '9', 'E']
 
 def draw_controller(screen, width, button, x, y):
 	# Clear screen
@@ -60,16 +38,27 @@ def draw_controller(screen, width, button, x, y):
 	screen.text("Intellivision 1", 0, 0, 1)
 	screen.hline(0, 12, width, 1)
 
-	for i in range(0, 3):
-		for j in range(0, 4):
-			index = (i * 4) + j
-			sx = x + 34 + (j * 10)
-			sy = y + 12 + (i * 9)
-			screen.rect(sx - 2, sy - 2, 9, 7, 1)
-			screen.rect(sx - 1, sy - 1, 7, 5, 1)
-			matrix[index].render(screen, sx, sy, False, True)
-			#screen.rect(sx - 2, sy - 2, 9, 7, 1)
-			screen.circle(17,39, 12, 1)
+	for i in range(3):
+		for j in range(4):
+			bevelled_rect(screen, x + KEYPAD_X_OFFSET + (i * 12), y + (j * 12), 8, 10, 2, 1)
+			screen.text(matrix[(i * 4) + j], x + KEYPAD_X_OFFSET + (i * 12) + 3, y + (j * 12) + 2, 1)
+
+	# Verticl Separator
+	screen.vline(width // 2, 22, 36, 1)
+	
+	# Action Buttons
+
+	# Top Buttons - These work as one
+	screen.rect(x + LEFT_ACTION_X_OFFSET, y + 13, 5, 9, 1)
+	screen.rect(x + RIGHT_ACTION_X_OFFSET, y + 13, 5, 9, 1)
+
+	# Bottom Buttons - These are individual
+	screen.rect(x + LEFT_ACTION_X_OFFSET, y + 25, 5, 9, 1)	
+	screen.rect(x + RIGHT_ACTION_X_OFFSET, y + 25, 5, 9, 1)
+
+	# Disc
+	screen.circle(x + DISC_X_OFFSET, y + DISC_Y_OFFSET, 20, 1)
+	screen.circle(x + DISC_X_OFFSET, y + DISC_Y_OFFSET, 18, 1)	
 
 def display_intv(screen, width, button, x, y):	
 	# Allow for button release
